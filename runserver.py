6fb618fccc7f5c298e94ffc01a18ae242db3d739
Ubuntu <ubuntu@ip-172-31-28-79.us-west-2.compute.internal>
2017-11-05 22:24:37 +0000

    changed ip address

--- runserver.py
+++ runserver.py
@@ -3,10 +3,13 @@ This script runs the jaddispainting application using a development server.
 """
 
 from os import environ
+import socket
 from jaddispainting import app
 
+ip = socket.gethostbyname(socket.gethostname())
+
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('SERVER_HOST', ip)
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
